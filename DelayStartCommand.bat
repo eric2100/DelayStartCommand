979e2517270a461e982d2bcc79441e17e5f8b3ff
Tsunghan,Tsai <adsl.cai@gmail.com>
2019-06-05 14:20:44 +0800

    Update DelayStartCommand.bat
    修正網路芳鄰沒有寫入權限
    增加設定開放防火牆

--- DelayStartCommand.bat
+++ DelayStartCommand.bat
@@ -1,9 +1,13 @@
 ::開啟網路芳鄰分享目錄
-net share TDDownload=X:\Dropbox\TDDownload /remark:"TDDownload" 
-net share Software=X:\Dropbox\Software /remark:"Software" 
+net share TDDownload=X:\Dropbox\TDDownload /remark:"TDDownload"  /GRANT:Everyone,FULL
+net share Software=X:\Dropbox\Software /remark:"Software"  /GRANT:Everyone,FULL
 
 ::啟動常用軟體
 start "" "X:\Dropbox\Software\桌面程式\Stickies9\stickies.exe"
 start "" "X:\Dropbox\Software\桌面程式\T-Clock\Clock64.exe"
 start "" "C:\Program Files (x86)\Dropbox\Client\Dropbox.exe"
 start "" %userprofile%\desktop\MaxLauncher.lnk
+
+:setWindowsFirewall
+@netsh advfirewall firewall add rule name="允許 stickies 連入" protocol=TCP dir=in localport=3306 action=allow  
+@netsh advfirewall firewall add rule name="允許 stickies 連出" protocol=TCP dir=out localport=3306 action=allow  
